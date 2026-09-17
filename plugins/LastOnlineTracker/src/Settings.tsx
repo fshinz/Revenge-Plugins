acea9be3d7c6d355fc20dd648ec5b19131ed6435
shin <mshin3400@gmail.com>
2026-09-17 05:26:03 +0300

    Update Settings.tsx

--- plugins/LastOnlineTracker/src/Settings.tsx
+++ plugins/LastOnlineTracker/src/Settings.tsx
@@ -6,27 +6,28 @@ import { ReactNative } from "@vendetta/metro/common";
 const { ScrollView, View } = ReactNative;
 
 export default function Settings() {
-    useProxy(storage);
+useProxy(storage);
 
-    return (
-        <ScrollView>
-            <View>
-                <Forms.FormSwitchRow
-                    label="Show Last Online on DM Header"
-                    value={storage.dmTopBar ?? true}
-                    onValueChange={v => storage.dmTopBar = v}
-                />
-                <Forms.FormSwitchRow
-                    label="Show Last Online in Member/DM List"
-                    value={storage.userList ?? true}
-                    onValueChange={v => storage.userList = v}
-                />
-                <Forms.FormSwitchRow
-                    label="Show Last Online on User Profiles"
-                    value={storage.profileUsername ?? true}
-                    onValueChange={v => storage.profileUsername = v}
-                />
-            </View>
-        </ScrollView>
-    );
+return (    
+    <ScrollView>    
+        <View>    
+            <Forms.FormSwitchRow    
+                label="Show Last Online on DM Header"    
+                value={storage.dmTopBar ?? true}    
+                onValueChange={v => storage.dmTopBar = v}    
+            />    
+            <Forms.FormSwitchRow    
+                label="Show Last Online in Member/DM List"    
+                value={storage.userList ?? true}    
+                onValueChange={v => storage.userList = v}    
+            />    
+            <Forms.FormSwitchRow    
+                label="Show Last Online on User Profiles"    
+                value={storage.profileUsername ?? true}    
+                onValueChange={v => storage.profileUsername = v}    
+            />    
+        </View>    
+    </ScrollView>    
+);
+
 }
